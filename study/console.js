c2ac26934e83ba302d1435b4d6c525fef2381c6f
Yongbb_13 <hvyongkim2@naver.com>
2024-03-15 12:01:03 +0900

    js study 0315

---
+++ study/console.js
@@ -0,0 +1,5 @@
+console.log('로그에 출력할 값');
+console.log(1);
+console.log(true);
+console.log({name: '김주하', age: 32, married: true});
+console.log('Juha', 24, true, [1,2,3]); 
---
+++ study/variable.js
@@ -0,0 +1,8 @@
+const player = '김주하';
+let number = 24;
+
+console.log(player, number);
+// player = '황연주'; -> 변할 수 없는 변수 오류발생.
+number = 11;
+
+console.log(player, number);
